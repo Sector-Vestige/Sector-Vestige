f6af0582734cb164466fce2a4c8b1159f8d2e117
Zamshi8 <235235723+Zamshi8@users.noreply.github.com>
2026-01-26 02:26:16 +1100

    Forgot to do some undos

--- Content.Shared/Whitelist/EntityWhitelistSystem.cs
+++ Content.Shared/Whitelist/EntityWhitelistSystem.cs
@@ -1,25 +1,7 @@
-// SPDX-FileCopyrightText: 2026 Sector-Vestige contributors
-// SPDX-FileCopyrightText: 2024 Errant <35878406+Errant-4@users.noreply.github.com>
-// SPDX-FileCopyrightText: 2024 Plykiya <58439124+Plykiya@users.noreply.github.com>
-// SPDX-FileCopyrightText: 2024 ShadowCommander <10494922+ShadowCommander@users.noreply.github.com>
-// SPDX-FileCopyrightText: 2024 deltanedas <39013340+deltanedas@users.noreply.github.com>
-// SPDX-FileCopyrightText: 2024 deltanedas <@deltanedas:kde.org>
-// SPDX-FileCopyrightText: 2024 plykiya <plykiya@protonmail.com>
-// SPDX-FileCopyrightText: 2024 psykana <36602558+psykana@users.noreply.github.com>
-// SPDX-FileCopyrightText: 2024 username <113782077+whateverusername0@users.noreply.github.com>
-// SPDX-FileCopyrightText: 2024 whateverusername0 <whateveremail>
-// SPDX-FileCopyrightText: 2025 Aiden <28298836+Aidenkrz@users.noreply.github.com>
-// SPDX-FileCopyrightText: 2025 Leon Friedrich <60421075+ElectroJr@users.noreply.github.com>
-// SPDX-FileCopyrightText: 2025 ReboundQ3 <ReboundQ3@gmail.com>
-// SPDX-FileCopyrightText: 2025 metalgearsloth <31366439+metalgearsloth@users.noreply.github.com>
-// SPDX-FileCopyrightText: 2025 slarticodefast <161409025+slarticodefast@users.noreply.github.com>
-// SPDX-FileCopyrightText: 2026 Zamshi8 <235235723+Zamshi8@users.noreply.github.com>
-//
-// SPDX-License-Identifier: MIT
-
 using System.Diagnostics.CodeAnalysis;
 using Content.Shared.Item;
 using Content.Shared.Tag;
+using Robust.Shared.Utility;
 
 namespace Content.Shared.Whitelist;
 
@@ -63,21 +45,13 @@ public sealed class EntityWhitelistSystem : EntitySystem
     /// </summary>
     public bool IsValid(EntityWhitelist list, EntityUid uid)
     {
-        if (list.Components != null)
-        {
-            if (list.Registrations == null)
-            {
-                var regs = StringsToRegs(list.Components);
-                list.Registrations = new List<ComponentRegistration>();
-                list.Registrations.AddRange(regs);
-            }
-        }
+        list.Registrations ??= StringsToRegs(list.Components);
 
-        if (list.Registrations != null && list.Registrations.Count > 0)
+        if (list.Registrations != null)
         {
             foreach (var reg in list.Registrations)
             {
-                if (HasComp(uid, reg.Type))
+                if (EntityManager.HasComponent(uid, reg))
                 {
                     if (!list.RequireAll)
                         return true;
@@ -151,61 +125,18 @@ public sealed class EntityWhitelistSystem : EntitySystem
         return !IsValid(whitelist, uid);
     }
 
-    /// <summary>
-    /// Helper function to determine if Blacklist is not null and entity is on list
-    /// Duplicate of equivalent Whitelist function
-    /// </summary>
-    public bool IsBlacklistPass(EntityWhitelist? blacklist, EntityUid uid)
+    private List<ComponentRegistration>? StringsToRegs(string[]? input)
     {
-        return IsWhitelistPass(blacklist, uid);
-    }
-
-    /// <summary>
-    /// Helper function to determine if Blacklist is not null and entity is not on the list
-    /// Duplicate of equivalent Whitelist function
-    /// </summary>
-    public bool IsBlacklistFail(EntityWhitelist? blacklist, EntityUid uid)
-    {
-        return IsWhitelistFail(blacklist, uid);
-    }
-
-    /// <summary>
-    /// Helper function to determine if Blacklist is either null or the entity is on the list
-    /// Duplicate of equivalent Whitelist function
-    /// </summary>
-    public bool IsBlacklistPassOrNull(EntityWhitelist? blacklist, EntityUid uid)
-    {
-        return IsWhitelistPassOrNull(blacklist, uid);
-    }
-
-    /// <summary>
-    /// Helper function to determine if Blacklist is either null or the entity is not on the list
-    /// Duplicate of equivalent Whitelist function
-    /// </summary>
-    public bool IsBlacklistFailOrNull(EntityWhitelist? blacklist, EntityUid uid)
-    {
-        return IsWhitelistFailOrNull(blacklist, uid);
-    }
-
-    private List<ComponentRegistration> StringsToRegs(string[]? input)
-    {
-        var list = new List<ComponentRegistration>();
-
         if (input == null || input.Length == 0)
-            return list;
+            return null;
 
+        var list = new List<ComponentRegistration>(input.Length);
         foreach (var name in input)
         {
-            var availability = Factory.GetComponentAvailability(name);
-            if (Factory.TryGetRegistration(name, out var registration)
-                && availability == ComponentAvailability.Available)
-            {
+            if (Factory.TryGetRegistration(name, out var registration))
                 list.Add(registration);
-            }
-            else if (availability == ComponentAvailability.Unknown)
-            {
-                Log.Error($"StringsToRegs failed: Unknown component name {name} passed to EntityWhitelist!");
-            }
+            else if (Factory.GetComponentAvailability(name) != ComponentAvailability.Ignore)
+                Log.Error($"{nameof(StringsToRegs)} failed: Unknown component name {name} passed to EntityWhitelist!");
         }
 
         return list;
